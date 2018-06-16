5629bc797aaf9683a3f9a8f2f31944ad2aced4ae
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-16 13:47:32 +0200

    fix bug

--- src/js/sagas/areListsFetched.js
+++ src/js/sagas/areListsFetched.js
@@ -1,5 +1,7 @@
 import {select} from 'redux-saga/effects';
 
+import {fetchingErrorSelector} from '../sagasSelectors/initialState';
+
 export default function *areListsFetched() {
 
     const fetchingError = yield select(fetchingErrorSelector);
