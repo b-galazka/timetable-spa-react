280811d630382dd4f4f97d622017a96213b63e7f
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-22 15:11:04 +0200

    add TimetableHeader props validation

--- src/js/components/TimetableHeader.jsx
+++ src/js/components/TimetableHeader.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 import urlTranslations from '../../json/urlTranslations';
 
@@ -53,5 +54,24 @@ class TimetableHeader extends Component {
         return decodeURIComponent(this.props.match.params.slug);
     }
 }
+
+TimetableHeader.propTypes = {
+
+    // router
+    match: propTypes.shape({
+        params: propTypes.shape({
+            slug: propTypes.string.isRequired,
+            type: propTypes.string.isRequired
+        }).isRequired
+    }).isRequired,
+
+    // redux
+    teachers: propTypes.arrayOf(
+        propTypes.shape({
+            slug: propTypes.string.isRequired,
+            name: propTypes.string
+        }).isRequired
+    ).isRequired,
+};
 
 export default withRouter(connect(mapStateToProps)(TimetableHeader));
